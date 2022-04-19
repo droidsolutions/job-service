6d499d6f4ce977bcdffbc01123acbba08092a72d
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2022-04-19 12:59:14 +0200

    fix(NPM): add export for IJobRepository

--- src/NodeJS/Index.ts
+++ src/NodeJS/Index.ts
@@ -1,4 +1,5 @@
 export type { IJob } from "./Generated/IJob";
+export type { IJobRepository } from "./Generated/IJobRepository";
 export { JobState } from "./Generated/JobState";
 export type { IJobWorkerBase } from "./Generated/Worker/IJobWorkerBase";
 export type { IJobWorkerSettings } from "./Generated/Worker/Settings/IJobWorkerSettings";
