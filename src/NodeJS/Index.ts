87ff1b5394c7efe937233bdecb78ee99b3524092
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2022-04-19 13:21:50 +0200

    feat(NPM): export LoggerFactgory and camcellationError typeguard

--- src/NodeJS/Index.ts
+++ src/NodeJS/Index.ts
@@ -3,4 +3,7 @@ export type { IJobRepository } from "./Generated/IJobRepository";
 export { JobState } from "./Generated/JobState";
 export type { IJobWorkerBase } from "./Generated/Worker/IJobWorkerBase";
 export type { IJobWorkerSettings } from "./Generated/Worker/Settings/IJobWorkerSettings";
+export { EmtpyLogger } from "./Helper/LoggerFactory";
+export type { LoggerFactory, SimpleLogger } from "./Helper/LoggerFactory";
+export { isCancellationError } from "./Helper/TypeGuards";
 export { JobWorkerBase } from "./JobWorkerBase";
