209982581960ec4e7361ec556e51be4ebbee2052
Stefan Ißmer <stefan.issmer@droidsolutions.de>
2023-09-12 12:35:11 +0200

    chore: mark transformDateToUtc as depcrecated

--- src/NodeJS/Helper/TimeHelper.ts
+++ src/NodeJS/Helper/TimeHelper.ts
@@ -4,6 +4,7 @@ import { addMinutes } from "date-fns";
  * Gets the given date in UTC.
  * @param {Date} date The date.
  * @returns {Date} The current date in UTC.
+ * @deprecated Use `new Date()` instead
  */
 export const transformDateToUtc = (date: Date = new Date()): Date => {
   return addMinutes(date, date.getTimezoneOffset());
